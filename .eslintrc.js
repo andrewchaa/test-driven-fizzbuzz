1cb466c719900b691b4d0e0f5e3f8743e39ff483
Andrew Chaa <andrew.yh.chaa@gmail.com>
2020-01-01 09:58:38 +0000

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 6,
   },
   env: {
     node: true
   },
+  plugins: ['prettier'],
   rules: {
     quotes: ['error', 'single', {avoidEscape: true}],
     'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': 'error',
   }
 }
