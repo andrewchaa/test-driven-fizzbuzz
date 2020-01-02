fbbb6effd458df94dd23924aa075a11215c66c97
Andrew Chaa <andrew.yh.chaa@gmail.com>
2020-01-02 07:48:28 +0000

    first enzyme test

--- carousel/src/tests/hello.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-describe('JSX', () => {
-  it('calls React.createElement', () => {
-    const createElementSpy = jest.spyOn(React, 'crateElement')
-    <h1>Hello, JSX!</h1>;
-    expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hello, JSX!')
-  })
-})
