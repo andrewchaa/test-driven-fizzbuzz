9a48c1999d2f83af40dbb2498a67ad946ddae2ed
Andrew Chaa <andrew.yh.chaa@gmail.com>
2020-01-14 07:39:52 +0000

    carousel slide

---
+++ carousel/src/tests/CarouselSlide.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import CarouselSlide from '../CarouselSlide'
+
+describe('CarouselSlide', () => {
+
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<CarouselSlide />)
+  })
+
+  it('renders a <figure>', () => {
+    expect(wrapper.type()).toBe('figure')
+  })
+
+  it('renders an <img> and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe('img')
+    expect(wrapper.childAt(1).type()).toBe('figcaption')
+  })
+
+  it('passes `imgUrl` through to the <img>', () => {
+    const imgUrl = 'https://example.com/image.png'
+    wrapper.setProps({ imgUrl })
+    const img = wrapper.find('img')
+    expect(img.prop('src')).toBe(imgUrl)
+  })
+})
